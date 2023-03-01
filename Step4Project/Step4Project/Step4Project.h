b4266486778291f0ec885e93a9af3eabf79c730b
kkchae <69879923+kkchae@users.noreply.github.com>
2023-03-01 11:15:37 +0900

    Step4 진행을 위한 솔루션 및 프로젝트 최초 생성
    * Step4 진행을 위한 프로젝트(Step4Project) 생성
     - MFC 다이얼로그 기반
    
    * 불필요 파일 업로드 방지를 위한 .gitignore 수정
    
    * 프로젝트 속성 변경
     - 출력 파일 디렉토리를 프로젝트 하위 output 폴더로 수정
     - 중간 파일 디렉토리를 프로젝트 하위 obj 폴더로 수정

---
+++ Step4Project/Step4Project/Step4Project.h
@@ -0,0 +1,32 @@
+﻿
+// Step4Project.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CStep4ProjectApp:
+// 이 클래스의 구현에 대해서는 Step4Project.cpp을(를) 참조하세요.
+//
+
+class CStep4ProjectApp : public CWinApp
+{
+public:
+	CStep4ProjectApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CStep4ProjectApp theApp;
